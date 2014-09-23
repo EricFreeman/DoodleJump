22666d117dfc178149a01015896036a359b758b2
Eric Freeman <eric.freeman@extendhealth.com>
2014-09-22 18:41:16 -0600

    Made parachutes and rockets work well (although the code is disgusting and needs a major refactor)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,21 +1,89 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts.Models;
+using Assets.Scripts.Models.Upgrades;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
     public class Player : MonoBehaviour
     {
         public float Speed = .1f;
+        public float RocketLength = 1;
 
-        void Update()
+        public int RemainingRockets;
+        public int RemainingParachutes;
+
+        private float _rocketStart;
+        private bool _isRocketFiring;
+        private float _parachuteStart;
+        private bool _isParachuteLaunched;
+
+        private void Start()
         {
-            var speed = Input.GetAxisRaw("Horizontal") * Speed;
+            RemainingRockets = PlayerContext.Get(UpgradeType.Rocket);
+            RemainingParachutes = PlayerContext.Get(UpgradeType.Parachute);
+        }
+
+        private void Update()
+        {
+            ApplyMovementSpeed();
+            VerifyAndUpdatePlayerX();
+            ManageRocket();
+            ManageParachute();
+        }
+
+        private void ManageRocket()
+        {
+            if (_isParachuteLaunched) return;
+            if (!_isRocketFiring)
+            {
+                if (RemainingRockets <= 0) return;
+                if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    _isRocketFiring = true;
+                    _rocketStart = Time.fixedTime;
+                    RemainingRockets--;
+                }
+            }
+            else
+            {
+                rigidbody.AddForce(0, 25, 0);
+                if (Time.fixedTime > _rocketStart + 1) _isRocketFiring = false;
+            }
+        }
+
+        private void ManageParachute()
+        {
+            if (_isRocketFiring) return;
+            if (!_isParachuteLaunched)
+            {
+                if (RemainingParachutes <= 0 || rigidbody.velocity.y > 0) return;
+                if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    _isParachuteLaunched = true;
+                    _parachuteStart = Time.fixedTime;
+                    RemainingParachutes--;
+                }
+            }
+            else
+            {
+                rigidbody.velocity = new Vector3(rigidbody.velocity.x, -.5f, 0);
+                if (Time.fixedTime > _parachuteStart + 1) _isParachuteLaunched = false;
+            }
+        }
+
+        private void ApplyMovementSpeed()
+        {
+            var speed = Input.GetAxisRaw("Horizontal")*Speed;
             rigidbody.AddForce(speed, 0, 0);
 
             var tiltSpeed = GetTiltSpeed();
             rigidbody.AddForce(tiltSpeed * Speed);
+        }
 
-            if(transform.position.x < -6) transform.Translate(-transform.position.x + 6, 0, 0);
-            if(transform.position.x > 6) transform.Translate(-transform.position.x - 6, 0, 0);
+        private void VerifyAndUpdatePlayerX()
+        {
+            if (transform.position.x < -6) transform.Translate(-transform.position.x + 6, 0, 0);
+            if (transform.position.x > 6) transform.Translate(-transform.position.x - 6, 0, 0);
         }
 
         private Vector3 GetTiltSpeed()
