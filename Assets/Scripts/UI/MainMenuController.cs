378b155e0a364f6a077ac783d4f95b10ba579618
Eric Freeman <eric.freeman@extendhealth.com>
2014-09-29 16:37:56 -0600

    Added a level for the main menu with buttons to take you to game, options (not made yet), and quit.

---
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        public void PlayButtonClicked()
+        {
+            Application.LoadLevel("Store");
+        }
+
+        public void OptionsButtonClicked()
+        {
+            Application.LoadLevel("Options");
+        }
+
+        public void QuitButtonClicked()
+        {
+            Application.Quit();
+        }
+    }
+}
