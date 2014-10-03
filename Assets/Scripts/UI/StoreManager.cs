ce7280bf2011fff2f281fad19d7788cfc5e1ab9e
Eric Freeman <eric.freeman@extendhealth.com>
2014-10-02 20:01:32 -0600

    Store loads, positions, and scales the upgrade panel list correctly now.

--- Assets/Scripts/UI/StoreManager.cs
+++ Assets/Scripts/UI/StoreManager.cs
@@ -9,19 +9,30 @@ namespace Assets.Scripts.UI
     public class StoreManager : MonoBehaviour
     {
         public GameObject UpgradePanelPrefab;
+        public GameObject UpgradeItemList;
 
         void Start()
         {
+            // find upgrades
             var upgrades = Assembly.GetExecutingAssembly()
                 .GetTypes()
                 .Where(x => x.BaseType == typeof (Upgrade));
+            var index = 0;
 
+            // add upgrades to store
             foreach (var u in upgrades)
             {
-                var prefab = (GameObject)Instantiate(UpgradePanelPrefab);
-                prefab.transform.SetParent(transform, false);
-                prefab.GetComponent<StoreUpgrade>().Setup((Upgrade)Activator.CreateInstance(u));
+                var prefab = (GameObject) Instantiate(UpgradePanelPrefab);
+                prefab.transform.SetParent(UpgradeItemList.transform, false);
+                prefab.GetComponent<StoreUpgrade>().Setup((Upgrade) Activator.CreateInstance(u));
+                prefab.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -index*100);
+                index++;
             }
+
+            // resize upgrade content panel so scorlling works correctly
+            var r = UpgradeItemList.GetComponent<RectTransform>();
+            r.sizeDelta = new Vector2(-25, index * 100);
+            r.rect.Set(0, 0, r.rect.width, r.rect.height);
         }
     }
 }
