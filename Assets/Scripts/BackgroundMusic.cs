24f2079daa17826ecf57d6c31e647d95628db3ab
Eric Freeman <eric.freeman@extendhealth.com>
2014-10-03 07:52:04 -0600

    Added a couple of new background songs and added an object to the scene that can't be destroyed on load to play it.  This keeps the music playing when switching levels.

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class BackgroundMusic : MonoBehaviour
+    {
+        public List<AudioClip> Songs;
+
+        private int _lastSong;
+
+        void Start()
+        {
+            DontDestroyOnLoad(gameObject);
+            if(!AlreadyExists())
+                PlayNewSong();
+        }
+
+        void Update()
+        {
+            if(!audio.isPlaying) PlayNewSong();
+        }
+
+        private bool AlreadyExists()
+        {
+            // because this object isn't destroyed when switching levels,
+            // there should already be one in the scene on load unless
+            // you're starting a specific level from the editor
+
+            if (FindObjectsOfType<BackgroundMusic>().Count() > 1)
+            {
+                DestroyImmediate(gameObject);
+                return true;
+            }
+
+            return false;
+        }
+
+        private void PlayNewSong()
+        {
+            var newSong = -1;
+            var rand = new System.Random(DateTime.Now.Millisecond);
+
+            while (newSong != _lastSong)
+                newSong = rand.Next(0, Songs.Count);
+
+            audio.clip = Songs[newSong];
+            audio.Play();
+            _lastSong = newSong;
+        }
+    }
+}
