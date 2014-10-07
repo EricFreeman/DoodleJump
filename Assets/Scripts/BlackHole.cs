84a1c1edff48f30945db9b2325d5dee870efaff4
Eric Freeman <eric.freeman@extendhealth.com>
2014-10-07 16:47:38 -0600

    Added a new type of enemy called blackhole that stays still but will try to suck in player if they get too close.

---
+++ Assets/Scripts/BlackHole.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class BlackHole : MonoBehaviour
+    {
+        private Player _player;
+        public float Speed;
+        public float Distance;
+
+        void Update()
+        {
+            if (_player == null) _player = FindObjectOfType<Player>();
+            else
+            {
+                if(Math.Abs(Vector3.Distance(_player.transform.position, transform.position)) < Distance)
+                    _player.transform.position = Vector3.MoveTowards(
+                        _player.transform.position,
+                        transform.position,
+                        Speed*Time.deltaTime);
+            }
+        }
+    }
+}
