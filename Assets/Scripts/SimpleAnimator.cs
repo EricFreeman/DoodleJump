f9c33026871174ce5ef3922465103123a3f4def8
Eric Freeman <eric.freeman@extendhealth.com>
2014-11-16 20:22:08 -0700

    Blurred background image so it didn't look so horrible and added script for animating any sprite renderer (used now for bird enemies making them the crappy bird guy flapping their wings.

---
+++ Assets/Scripts/SimpleAnimator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Linq;
+
+namespace Assets.Scripts
+{
+    public class SimpleAnimator : MonoBehaviour
+    {
+        public Sprite[] Frames;
+        private int _frame;
+        public int SpriteSpeed;
+        private int _currentSpriteSpeed;
+
+        void Update()
+        {
+            if (_currentSpriteSpeed-- <= 0)
+            {
+                _currentSpriteSpeed = SpriteSpeed;
+                if (_frame++ >= Frames.Count() - 1) _frame = 0;
+                GetComponent<SpriteRenderer>().sprite = Frames[_frame];
+            }
+        }
+    }
+}
